e0b2fd26206d3658bff361b4b2058218006f2172
LoMi <mlomovic@gmail.com>
2020-03-01 09:38:05 +0100

    1. Prvi komit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import RegistrationForm, LoginForm
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f7446e435991868183813c59f0307b58'
+
+posts = [
+    {
+        'author' : 'petar petrovic',
+        'title' : 'blog 1',
+        'content' : 'Prvi post',
+        'date_posted' : 'April 4, 2018'
+    },
+    {
+        'author' : 'mika mikic',
+        'title' : 'blog 2',
+        'content' : 'Drugi post',
+        'date_posted' : 'June 18, 2019'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+
+    return render_template('about.html', title='about')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        #flash('Account created for {}!'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please try again', 'danger')
+    
+    return render_template('login.html', title="Register", form=form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
